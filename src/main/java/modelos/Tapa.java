97d5257b552f240e288d1eb5c1881b35cef28dd6
ikari <ikari1607@gmail.com>
2018-10-17 21:34:33 -0300

    nueva clase Tapa

---
+++ src/main/java/modelos/Tapa.java
@@ -0,0 +1,27 @@
+package modelos;
+
+public class Tapa {
+    private String nombre;
+    private String descripcion;
+
+    public Tapa(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
