f900d614c87f30d384418bb99ffa812e61b83de9
Lisa <lisaevemarin17@gmail.com>
2018-10-19 23:12:14 -0300

    modificacion VistaTapaMenu ModeloTapa Tapa

--- src/main/java/vistas/VistaTapa.java
+++ src/main/java/vistas/VistaTapa.java
@@ -1,7 +1,6 @@
 package vistas;
 
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
