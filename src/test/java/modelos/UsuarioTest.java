4b87a1fff651f57e59abb5d7bc891b37a682d024
OlmosLautaroGaston <lautaolmoss@gmail.com>
2019-08-23 21:56:25 -0300

    VERSION FUNCIONAL DEL PROYECTO

--- src/test/java/modelos/UsuarioTest.java
+++ src/test/java/modelos/UsuarioTest.java
@@ -8,29 +8,5 @@ import static org.junit.Assert.*;
 
 public class UsuarioTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-    @Test
-    public void testComprobarUsuarioVacio(){
-        Usuario user= new Usuario("","");
-        boolean result = user.comprobarUsuario(user);
-        assertEquals(false,result);
-    }
-    @Test
-    public void testComprobarUsuarioRelleno(){
-        Usuario user= new Usuario("coso","unodostres");
-        boolean result = user.comprobarUsuario(user);
-        assertEquals(true,result);
-    }
-    @Test
-    public void testComprobarContraseña(){
-        Usuario user = new Usuario("coso","coso");
-        boolean result = user.comprobarUsuario(user);
-        assertEquals(false,result);
-    }
+   
 }
